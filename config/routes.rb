a22f7416184429dd8418d1bbb700d8d8f55bff16
Denis Torgunov <D.Torgunov@student.bradford.ac.uk>
2015-03-05 23:04:56 +0000

    Added a basic controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/home' => "pages#home"
+  get '/shops' => "shops#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
