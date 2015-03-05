a64bcded9b50361473ac205c7e91e33214329c83
Denis Torgunov <D.Torgunov@student.bradford.ac.uk>
2015-03-05 23:23:31 +0000

    Added an individual show view for each shop

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/home' => "pages#home"
-  get '/shops' => "shops#index"
   # The priority is based upon order of creation: first created -> highest priority.
+  resources :shops
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
