f139a7d14ac8f39400f4539323b1c4eeabb01339
Denis Torgunov <D.Torgunov@student.bradford.ac.uk>
2015-03-06 04:49:41 +0000

    Changed dummy data

--- transation_example.sh
+++ transation_example.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -i -X POST -d "{\"api_token\":\"42\",\"customer\":1,\"quantity\":1}" localhost:3000/api/purchase/3
+curl -i -X POST -d "{\"api_token\":\"42\",\"customer\":5432567,\"quantity\":1}" localhost:3000/api/purchase/7
